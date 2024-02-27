2a70cded7ddb4df69a4d73b8bb0bda6c9ec265f8
hzeghouani <hzeghouani@hzeghouanis-MBP.lan>
2024-02-27 20:59:12 +0100

    push change

--- pages/explication.php
+++ pages/explication.php
@@ -1,50 +1,7 @@
 <?php
     session_start();
 ?>
-
-<!doctype html>
-<html>
-	<head>
-		<title>Eveil ZooLangues</title>
-        <link rel="icon" type="image/x-icon" href="../medias/logo-petit.png">
-		<link rel="StyleSheet" type="text/css" href="../style/style.css"/>
-		<meta charset="utf-8" />
-	</head>
-    <header>
-        <div id="logo">
-            <a href="../index.php">
-                <img src="../medias/Logo.png" height="90">
-            </a>
-        </div>
-        <div class="deroule-menu" id="ressources">
-            <a href="ressources.php"><button class="deroule-menu">Ressources</button></a>
-            <div class="sous-menu">
-                <a href="ressources.php#approches-plurielles">Les Approches Plurielles</a>
-                <a href="ressources.php#evl">L'éveil aux langues</a>
-                <a href="ressources.php#applications">Contextes d'applicaiton</a>
-                <a href="ressources.php#plus-loin">Ressources pour aller plus loin</a>
-            </div>
-        </div>
-        <a href="explication.php" id="explication"><button>Notre Site</button></a>
-        <div class="deroule-menu" id="biblio">
-            <a href="biblio.php"><button class="deroule-menu">Bibliothèque</button></a>
-            <div class="sous-menu">
-                <a href="biblio-lire.php">Commencer à lire</a>
-            </div>
-        </div>
-        <div id="login">       
-            <div  class="deroule-menu">     
-                <a href="connexion-et-profil/profil.php">
-                    <img src="../medias/connexion.png" alt="Se connecter" height="50" width="50">
-                    Connexion
-                </a>
-                <div class="sous-menu" id="login-sous-menu">
-                    <a href="connexion-et-profil/connexion.php">Se connecter</a>
-                    <a href="connexion-et-profil/creation.php">Créer un compte</a>
-                </div>
-            </div>
-        </div>
-    </header>
+<?php include('../layout/header.php'); ?>
         <main>
             <div id="explication-haut">
             <h1>Bienvenue à EveilZooLangues !</h1>
