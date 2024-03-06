5d2f31d41d2917807055eae71eaeb13740f93923
hzeghouani <hzeghouani@hzeghouanis-MacBook-Pro.local>
2024-03-06 15:27:57 +0100

    final push

--- pages/systeme-de-connexion/connexion_ratee.php
+++ pages/systeme-de-connexion/connexion_ratee.php
@@ -4,49 +4,7 @@
     if(!isset($_SESSION['logged_user'])) :
 ?>
 <!doctype html>
-<html>
-	<head>
-		<title>Eveil ZooLangues</title>
-        <link rel="icon" type="image/x-icon" href="medias/logo-petit.png">
-		<link rel="StyleSheet" type="text/css" href="../../style/style.css"/>
-		<meta charset="utf-8" />
-	</head>
-    <body id="body-accueil" class="page-simple">
-        
-        <header>
-            <div id="logo">
-                <a href="../../index.php">
-                    <img src="../../medias/Logo.png" height="90">
-                </a>
-            </div>
-            <div class="deroule-menu" id="ressources">
-                <a href="ressources.php"><button class="deroule-menu">Ressources</button></a>
-                <div class="sous-menu">
-                    <a href="ressources.php#approches-plurielles">Les Approches Plurielles</a>
-                    <a href="ressources.php#evl">L'éveil aux langues</a>
-                    <a href="ressources.php#applications">Contextes d'applicaiton</a>
-                    <a href="ressources.php#plus-loin">Ressources pour aller plus loin</a>
-                </div>
-            </div>
-            <a href="explication.php" id="explication"><button>Notre Site</button></a>
-            <div class="deroule-menu" id="biblio">
-                <a href="biblio.php"><button class="deroule-menu">Bibliothèque</button></a>
-                <div class="sous-menu">
-                    <a href="biblio-lire.php">Commencer à lire</a>
-                </div>
-            </div>            <div id="login">       
-                <div  class="deroule-menu">     
-                    <a href="connexion.php">
-                        <img src="../../medias/connexion.png" alt="Se connecter" height="50" width="50">
-                        Connexion
-                    </a>
-                    <div class="sous-menu" id="login-sous-menu">
-                        <a href="connexion.php">Se connecter</a>
-                        <a href="creation.php">Créer un compte</a>
-                    </div>
-                </div>
-            </div>
-        </header>
+<?php include('header.php'); ?>
                 <div id="pageConnexion">
                     <section id="sectionConnexion">
                         <h1>
